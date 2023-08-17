06fc1a36dbe67d1b85485a7b467db7a84d2ce7fc
andres-bonilla <x.andresbonilla@gmail.com>
2023-08-17 13:36:25 -0300

    Actualizacion basica de dependencias y reestructuracion del proyecto

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Constants from "expo-constants";
+/* const { manifest } = Constants; */
+import { View, Text, SafeAreaView, LogBox } from "react-native";
+import * as React from "react";
+import RootNavigation from "./navigation/rootNavigation";
+import { StatusBar } from "expo-status-bar";
+import { Provider } from "react-redux";
+import store from "./state/store";
+
+function App() {
+  LogBox.ignoreAllLogs();
+  return (
+    <Provider store={store}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#0e0b29" }}>
+        <StatusBar style="light" />
+        <RootNavigation />
+      </SafeAreaView>
+    </Provider>
+  );
+}
+
+export default App;
